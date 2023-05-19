bc318ffa05a43ea7f2e8571dc4992d7ab5b7eea0
Donostia12 <suncry16@gmail.com>
2023-05-19 08:36:15 +0800

    nambah migrate

---
+++ database/migrations/2023_05_19_000622_berita.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('berita', function (Blueprint $table) {
+            $table->Increments('id_berita');
+            $table->string('judul_berita');
+            $table->text('isi_berita');
+            $table->timestamps();
+        });
+    }
+
+        
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::drop('berita');
+    }
+};
